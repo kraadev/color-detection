a66019c66957527089fd7bc38f1e570389dfdc81
kraadev <kraadev29@gmail.com>
2025-09-08 20:00:57 +0700

    fix: use public URL for og:image

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://color-detection-lyart.vercel.app', // nanti ganti kalau pakai domain custom
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
